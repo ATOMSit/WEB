65da9faf6ff78498fd477c6de079250ee8b80676
Rémy LENGLET <lenglet.remy@atomsit.com>
2019-09-30 18:29:58 +0200

    Add asset for admin theme

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'last_name', 'first_name', 'email', 'password',
     ];
 
     /**
